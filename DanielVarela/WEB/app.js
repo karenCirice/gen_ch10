008b3e97857dc30e4ef79d9ed42aafab496e963c
Daniel2104 <87034629+Daniel2104@users.noreply.github.com>
2022-03-03 23:13:18 -0600

    Add files via upload

---
+++ DanielVarela/WEB/app.js
@@ -0,0 +1,5 @@
+console.log("Imagenes en la pagina:",document.images);
+console.log("Links en la pagina:",document.links);
+console.log("Titulo de la pagina:",document.title);
+console.log("head de la pagina:",document.head);
+console.log("Body de la pagina:",document.body);
