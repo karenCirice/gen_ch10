65b1d9d1fcfe594dde52d84ef5a92878b09fea98
ernestoAld <ernestoaldanati@gmail.com>
2022-03-03 23:33:31 -0600

    añadiendo archivos ernestoAld

---
+++ ernestoAld/web1/ejercicios.js
@@ -0,0 +1,17 @@
+//Video 1
+let elemento;
+
+elemento = document;
+elemento = document.head;
+elemento = document.title;
+console.log(elemento);
+
+
+//video 2
+let elemento2 = document.links;//asi podemos buscar elementos dentro de nuestro document
+console.log(elemento2);
+
+
+// obtener total de imagenes de Yahoo.com
+let yahoo = document.images;
+console.log(`Yahoo.com tiene ${yahoo.length} imagenes`);
