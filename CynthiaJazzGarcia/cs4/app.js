8c0df1e04313af1fd8fcc917d619c638ec18adee
Cynthia Jazz Garcia <cynthiajazz93@gmail.com>
2022-03-04 22:38:55 -0600

    FEAT: Se agrega la carpeta de CynthiaJazzGarcia

---
+++ CynthiaJazzGarcia/cs4/app.js
@@ -0,0 +1,9 @@
+let arr = ["This", "is", "a", "sentence."];
+function printOutString() {
+  let arr = ["This", "is", "a", "sentence."];
+for (let i=0; i<arr.length; i++) {
+    console.log(arr[i]);
+}
+}
+ 
+printOutString();
