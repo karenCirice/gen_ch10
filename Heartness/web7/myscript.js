780b0d021120dc8e0c459a1d2a3fdab673522f30
Eddie <eddie.edward97@gmail.com>
2022-03-14 11:19:19 -0600

    FEAT:cambios a web7

--- Heartness/web7/myscript.js
+++ Heartness/web7/myscript.js
@@ -2,6 +2,11 @@ document.getElementById("demo").innerHTML = "My First JavaScript"
 
 function myFunction(){
     document.getElementById("demo").style.color = "red";
-    document.getElementById("demo").innerHTML = "Ahora el título es  ROJO"
-    console.log("myFunction se ejecutó correctamente")
+    document.getElementById("demo").innerHTML = "Ahora el título es  ROJO";
+    console.log("myFunction se ejecutó correctamente");
+}
+
+function myFunction2(element, color){
+    element.style.color = color;
+    console.log("Datos de this: ", element);
 }
