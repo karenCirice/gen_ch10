d8f21e1a42db97c0ebdea26139a71daaced40e5c
Eddie <eddie.edward97@gmail.com>
2022-03-14 11:08:15 -0600

    FEAT: Ejercicio web7

---
+++ Heartness/web7/myscript.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML = "My First JavaScript"
+
+function myFunction(){
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").innerHTML = "Ahora el título es  ROJO"
+    console.log("myFunction se ejecutó correctamente")
+}
