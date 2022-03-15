2d3ac1a93dacd359a942d62acc3c0a7fd90fd50c
Gabriela Díaz Reyes <gabriela.d_reyes@outlook.com>
2022-03-15 00:20:37 -0600

    antes de mi merge

--- AliRolo-DOM/CS4/Ejercicio1.js
+++
@@ -1,7 +0,0 @@
-const arr = ["This", "is", "a", "sentence."];
-let sentence = ""
-function printOutString(word){
-sentence = sentence + word + " ";
-}
-arr.forEach(printOutString);
-console.log(sentence);
--- AliRolo-DOM/CS4/Ejercicio2.js
+++
@@ -1,9 +0,0 @@
-const nameArray2 = [5,6,7,8,9];
-const answerArray = [];
-function multiplicar(num) {
-answerArray.push(num * 2);
-}
-
-nameArray2.forEach(multiplicar);
-
-console.log(answerArray);
