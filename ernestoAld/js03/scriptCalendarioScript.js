3cb9832dfd01e4092d642af3dd505babbc176ecc
ernestoAld <ernestoaldanati@gmail.com>
2022-03-18 13:27:56 -0600

    UPDATING: index.html filename

--- ernestoAld/js03/scriptCalendarioScript.js
+++ ernestoAld/js03/scriptCalendarioScript.js
@@ -1,17 +1,13 @@
 let calcularFecha = ()=>{
-    
-    let fecha = new Date(parseInt(document.getElementById("anio").value),parseInt(document.getElementById("mes").value)-1,parseInt(document.getElementById("dia").value));    
+    let fecha = new Date(parseInt(document.getElementById("anio").value),parseInt(document.getElementById("mes").value)-1,parseInt(document.getElementById("dia").value));
     definirFecha(fecha.getDay());
-
 }
 
-
-
 function definirFecha(dia){
-    
+    // condicion ? resultado1 : resultado2
     switch(dia){
-        case 0,6:
-            dia === 0 ? document.getElementById("fechaCalculada").value = "DOMINGO, FIN DE SEMANA" : document.getElementById("fechaCalculada").value = "SABADO, FIN DE SEMANA";
+        case 0:
+            document.getElementById("fechaCalculada").value = "DOMINGO, FIN DE SEMANA";
             break;
         case 1:
             document.getElementById("fechaCalculada").value = "LUNES, DIA LABORABLE";
@@ -28,5 +24,8 @@ function definirFecha(dia){
         case 5:
             document.getElementById("fechaCalculada").value = "VIERNES, DIA LABORABLE";
             break;
+        case 6:
+            document.getElementById("fechaCalculada").value = "SABADO, FIN DE SEMANA";
+
     }
 }
