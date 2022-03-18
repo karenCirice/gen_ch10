9dbdc91797874089bbf6fecb32252dd33dd6c4c2
MarianaVives <marianavives123@gmail.com>
2022-03-18 16:29:32 -0600

    Fix calendar (add keys {} to switch, fix clear button, change.value in the switch area and also change the id calcular/weekday on the case

--- MarianaVives/JS03/index.js
+++ MarianaVives/JS03/index.js
@@ -9,59 +9,58 @@
 
 
 function calcular() {
-    let day = document.getElementById("day").value;
+    let day = parseInt(document.getElementById("day").value);
     console.log(day);
-    let month = document.getElementById("month").value;
+    let month = parseInt(document.getElementById("month").value)-1;
     console.log(month);
-    let year = document.getElementById("year").value;
+    let year = parseInt(document.getElementById("year").value);
     console.log(year);
 
     let fecha = new Date(day, month, year);
     console.log(fecha);
 
-    let nDay= d.getDay();
+    let nDay = fecha.getDay();
     console.log(nDay);
-}
 
-switch(nDay){
-    case 0:
-    document.getElementById("calcular").innerHTML="Lunes es día laborable";
-    break;
-}
-switch(nDay){
-    case 1:
-    document.getElementById("calcular").innerHTML="Martes es día laborable";
-    break;
-}
-switch(nDay){
-    case 2:
-    document.getElementById("calcular").innerHTML="Miércoles es día laborable";
-    break;
-}
-switch(nDay){
-    case 3:
-    document.getElementById("calcular").innerHTML="Jueves es día laborable";
-    break;
-}
-switch(nDay){
-    case 4:
-    document.getElementById("calcular").innerHTML="Viernes es día laborable";
-    break;
-}
-switch (nDay){
-    case 5:
-    document.getElementById("calcular").innerHTML="Sábado es día laborable";
-    break;
+    switch (nDay) {
+        case 0:
+            document.getElementById("weekday").value = "Lunes es día laborable";
+            break;
+    }
+    switch (nDay) {
+        case 1:
+            document.getElementById("weekday").value = "Martes es día laborable";
+            break;
+    }
+    switch (nDay) {
+        case 2:
+            document.getElementById("weekday").value = "Miércoles es día laborable";
+            break;
+    }
+    switch (nDay) {
+        case 3:
+            document.getElementById("weekday").value = "Jueves es día laborable";
+            break;
+    }
+    switch (nDay) {
+        case 4:
+            document.getElementById("weekday").value = "Viernes es día laborable";
+            break;
+    }
+    switch (nDay) {
+        case 5:
+            document.getElementById("weekday").value = "Sábado no es día laborable";
+            break;
 
-}
-switch(nDay){
-    case 6:
-    document.getElementById("calcular").innerHTML="Domingo es día laborable";
-    break;
-
+    }
+    switch (nDay) {
+        case 6:
+            document.getElementById("weekday").value = "Domingo no es día laborable";
+            break;
+    }
 }
 
 // CLEAR
-function clear () {
-    document.getElementById("calcular").reset();
+function clear() {
+    document.getElementById("weekday").value= "";
 }
