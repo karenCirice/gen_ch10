4c35e37da30ff5e9d040a1682055089152a32c0e
GeRayas <gerardo.rrp19@gmail.com>
2022-03-17 23:55:18 -0600

    feat: js03

--- GeRayas/js03/myscript.js
+++ GeRayas/js03/myscript.js
@@ -1,13 +1,39 @@
-//Función Calcular
+let resultado;
+//Función Calcular Dia De La Semana Laboral
 function calcular() {
     let día = parseInt(document.getElementById("día").value);
-    let mes = parseInt(document.getElementById("mes").value);
+    let mes = parseInt(document.getElementById("mes").value) - 1;
     let año = parseInt(document.getElementById("año").value);
-    let fecha = new Date(año,mes,día);
-    let resultado = fecha.getDay();
-    document.getElementById("resultado").value = resultado;
+    const fecha = new Date(año, mes, día);
+    resultado = fecha.getDay();
+    console.log(resultado);
+
+    switch (resultado) {
+        case 0, 6:
+            resultado === 0 ? document.getElementById("resultado").value = 'DOMINGO, FIN DE SEMANA' : document.getElementById("resultado").value = 'SABADO, FIN DE SEMANA';
+            break;
+        case 1:
+            document.getElementById("resultado").value = 'LUNES, DIA LABORAL';
+            break;
+        case 2:
+            document.getElementById("resultado").value = 'MARTES, DIA LABORAL';
+            break;
+        case 3:
+            document.getElementById("resultado").value = 'MIERCOLES, DIA LABORAL';
+            break;
+        case 4:
+            document.getElementById("resultado").value = 'JUEVES, DIA LABORAL';
+            break;
+        case 5:
+            document.getElementById("resultado").value = 'VIERNES, DIA LABORAL';
+            break;
+    }
 }
 /*FUNCIÓN LIMPIAR*/
-function limpiar(){
+function limpiar() {
     document.getElementById("Formulario").reset();
- }
+}
+
+
+
+
