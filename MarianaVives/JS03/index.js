0b858a8a846af112b98118248bb498038a19e257
MarianaVives <marianavives123@gmail.com>
2022-03-18 13:07:39 -0600

    add style to JS03 and more code

--- MarianaVives/JS03/index.js
+++ MarianaVives/JS03/index.js
@@ -1,19 +1,67 @@
-let d= new Date
+/**
+ * Valida si la información es correcta
+ * @param {number} day //Es el día de la fecha seleccionada
+ * @param {number} month // Es el mes de la fecha seleccionada
+ * @param {number} year // Es el año de la fecha seleccionada
+ * @returns {number} 
+ * 
+ */
 
-function day(){
-    let day= d.getDay()
-    document.getElementById("day").innerHTML = day;
+
+function calcular() {
+    let day = document.getElementById("day").value;
+    console.log(day);
+    let month = document.getElementById("month").value;
+    console.log(month);
+    let year = document.getElementById("year").value;
+    console.log(year);
+
+    let fecha = new Date(day, month, year);
+    console.log(fecha);
+
+    let nDay= d.getDay();
+    console.log(nDay);
 }
 
-function month(){
-    let month= d.getMonth()
-    document.getElementById("2").innerHTML = mes;
+switch(nDay){
+    case 0:
+    document.getElementById("calcular").innerHTML="Lunes es día laborable";
+    break;
+}
+switch(nDay){
+    case 1:
+    document.getElementById("calcular").innerHTML="Martes es día laborable";
+    break;
+}
+switch(nDay){
+    case 2:
+    document.getElementById("calcular").innerHTML="Miércoles es día laborable";
+    break;
+}
+switch(nDay){
+    case 3:
+    document.getElementById("calcular").innerHTML="Jueves es día laborable";
+    break;
+}
+switch(nDay){
+    case 4:
+    document.getElementById("calcular").innerHTML="Viernes es día laborable";
+    break;
+}
+switch (nDay){
+    case 5:
+    document.getElementById("calcular").innerHTML="Sábado es día laborable";
+    break;
+
+}
+switch(nDay){
+    case 6:
+    document.getElementById("calcular").innerHTML="Domingo es día laborable";
+    break;
+
 }
 
-function year(){
-    let year= d.getFullYear();
-    document.getElementById("3").innerHTML = year;
-    console.log
+// CLEAR
+function clear () {
+    document.getElementById("calcular").reset();
 }
-
-
