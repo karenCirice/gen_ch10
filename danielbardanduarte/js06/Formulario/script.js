2d51921071ff708648a484356994101ffe067bce
danielbardanduarte <danielbardanduarte@gmail.com>
2022-03-22 17:31:54 -0600

    update: Datos actualizados y completar js06

--- danielbardanduarte/js06/Formulario/script.js
+++ danielbardanduarte/js06/Formulario/script.js
@@ -10,4 +10,34 @@ function obtenerFormulario(){
       concatenacion = concatenacion + " " + i.value;  
     }
     console.log(concatenacion);
+    console.log(formulario.nombre.value);
+    console.log(formulario.apellido.value);
+    localStorage.setItem(formulario["nombre"]["name"], formulario["nombre"]["value"]);
+    localStorage.setItem(formulario["apellido"]["name"], formulario["apellido"]["value"]);
+}
+
+/**
+ * Pone invisible al boton principal
+ */
+function setInvisible(){
+    let botonFormulario = document.getElementById("todos");
+    botonFormulario.style.visibility = "hidden";
+}
+/**
+ * Oculta el botón principal del formulario
+ */
+function setOcultar(){
+    let botonFormulario = document.getElementById("todos");
+    botonFormulario.style.display = "none";
+}
+
+/**
+ * Recuperar datos de la memoria
+ */
+function recuperarFormulario(){
+    let formulario = document.forms["formulario"];
+    console.log("Datos en memoria: " ,localStorage.getItem(formulario["nombre"]["name"]), localStorage.getItem(formulario["apellido"]["name"]));
+
+    formulario["nombre"]["value"] = localStorage.getItem(formulario["nombre"]["name"]);
+    formulario["apellido"]["value"] = localStorage.getItem(formulario["apellido"]["name"]);
 }
