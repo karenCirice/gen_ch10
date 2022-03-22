83eef73ad945f90ee66a0f010a52d55306d0b479
Karen González <69605681+KarenGtz30@users.noreply.github.com>
2022-03-22 00:16:50 -0600

    FEAT: Termino de jso2 y se agrega jso3

--- KarenGtz30/web7/myScript.js
+++ KarenGtz30/web7/myScript.js
@@ -3,5 +3,5 @@ console.log("Iniciando script");
 function myFunction(){
     let primernombre = document.getElementById('nombre');
     let nombre = window.prompt("Cual es tu nombre?");
-    primernombre.innerHTML = "Hola " + nombre;
+    primernombre.innerHTML = `Hola ${nombre}!`;
 }
