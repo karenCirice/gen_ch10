f15169c93b598b51578bed3de02cbea903acd079
mauriciodavidlavider <m.davidestrella@gmail.com>
2022-03-23 11:27:32 -0600

    feat: agregando tabla json

---
+++ mauriciodavidlavider/js06/leerJson/script.js
@@ -0,0 +1,24 @@
+
+console.log("JS06 Leer JSON");
+
+fetch ("colors.json")
+.then ((response) => response.json())
+.then ((data) => {
+    console.log(data);
+    console.log(Array.isArray(data.colors));
+
+    let tBody = document.getElementById("tBody");
+
+    for(let color of data.colors){
+        console.log(color.color);
+        console.log(color.code.hex);
+        tBody.innerHTML += `
+            <tr>
+            <td>${color.color}</td>
+            <td>${color.category}</td>
+            <td>${color.type ? color.type : "Sin tipo"}</td>
+            <td>${color.code.hex}</td>
+            </tr>
+        `;
+    }
+})
