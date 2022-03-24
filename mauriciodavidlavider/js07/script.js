bfce2d8f1dcd42e49c2022110b384c34b3fe8ebf
mauriciodavidlavider <m.davidestrella@gmail.com>
2022-03-23 21:16:21 -0600

    feat: agregando lista empleados

--- mauriciodavidlavider/js07/script.js
+++ mauriciodavidlavider/js07/script.js
@@ -1,16 +1,39 @@
 
 console.log("js07");
 
-function busquedaUsuarios(page){
+function busquedaUsuarios(){
     console.log("Se activo boton de busqueda");
 
     //fetch(`https://regres.in/api/users?page=${page}`)
-    fetch(`https://regres.in/api/users/23`)
+    //fetch(`https://reqres.in/api/users/100`)
+    fetch(`https://reqres.in/api/users?page=2`)  //Regresa a los empleados de la pagina seleccionada
     .then( response => response.json())
     .then( users => {
-        console.log(users)
+        console.log(users.data); 
+/*      console.log(users.data.id); 
+        console.log(users.data.first_name); 
+        console.log(users.data.last_name); 
+        console.log(users.data.email); 
+        console.log(users.data.avatar); //imagen */
+    
+        let tBody = document.getElementById("tBody");
+
+        for(let element of users.data){
+            console.log(element);
+
+            tBody.innerHTML += `
+                <tr>
+                <td>${element.id}</td>
+                <td>${element.first_name}</td>
+                <td>${element.last_name}</td>
+                <td>${element.email}</td>
+                <td><img style="border-radius: 50%; width: 30%" src="${element.avatar}"</td>
+                </tr>
+            `;
+        }
+    
     })
     .catch( error => {
-        console.log("la solicitud causo error:", error)
+        console.log("la solicitud causo error:", error);
     })
 }
