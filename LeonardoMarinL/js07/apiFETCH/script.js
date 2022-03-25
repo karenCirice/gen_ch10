24656b3f82bcc0fc47243ab876eb4d0f8cac9753
Leonardo Marín López <leo.lml@gmail.com>
2022-03-24 18:27:36 -0600

    CHORE: actualización para añadir botones de atrás y adelante, aún no funcionan

--- LeonardoMarinL/js07/apiFETCH/script.js
+++ LeonardoMarinL/js07/apiFETCH/script.js
@@ -1,21 +1,23 @@
 console.log("js07 apiFETCH");
 
 /**
- * this function allows to recover the array from de api and print the values from its array "data", 
- * which contains user's information
+ * this function allows to recover the array from de api and asign them to the function tabla(data)
  */
-function busquedaUsuarios() {
-  for (let i=1; i<=2; i++){
-  fetch(`https://reqres.in/api/users?page=${i}`)
+let page;
+function busquedaUsuarios(page) {
+  fetch(`https://reqres.in/api/users?page=${page}`)
     .then((response) => response.json())
     .then((data) => tabla(data))
-  
+
     .catch((error) => {
       console.log("La solicitud causó error: ", error);
     });
-  }
 }
 
+/**
+ * this function allows to print the values from the array data on a table and deploy back and forward buttons
+ * @param {*} data 
+ */
 function tabla(data) {
   let contenido = document.getElementById("contenido");
   for (let cont of data.data) {
@@ -28,11 +30,42 @@ function tabla(data) {
         <td class="col sm-col-12"><img class="imagen" src="${cont.avatar}"></td>
     </tr>`;
   }
+
+  /**this is the creation of the Back button */
+  document.getElementById("adelante").innerHTML = `
+          <button
+            onclick="atras()"
+            type="button"
+            class="btn btn-success boton1"
+          >
+            <= Back
+          </button>`;
+  /**this is the creation of the Next button */
+  document.getElementById("atrás").innerHTML = `
+          <button
+            onclick="adelante()"
+            type="button"
+            class="btn btn-success boton1"
+          >
+            Next =>
+          </button>`;
 }
 
+function atras(){
+  document.page.innerHTML = -1;
+};
+
+function adelante(){
+  document.page.innerHTML = +1;
+};
+
 /**
- * this function allows to clean the table
+ * this function allows to clean the table and remove the back and forward buttons 
  */
 function limpiarUsuarios() {
   contenido.innerHTML = "";
+  let botonA =document.getElementById("adelante");
+  botonA.innerHTML ="";
+  let botonB = document.getElementById("atrás");
+  botonB.innerHTML ="";
 }
