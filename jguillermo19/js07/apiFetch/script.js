0e52c76acd3d1d870cbb817729136cbf7523f3e6
Guillermo Sandoval <jguillermosh19@gmail.com>
2022-03-28 10:54:53 -0600

    CHANGE: archivos de carpeta js07

--- jguillermo19/js07/apiFetch/script.js
+++ jguillermo19/js07/apiFetch/script.js
@@ -1,40 +1,29 @@
-console.log("JS07 apiFetch")
+console.log("Js07 apiFetch");
 
-function userList(page=1){
-    fetch(`https://reqres.in/api/users?page=${page}`)
-    .then(response => response.json())
-    .then(users => {
-        console.log(users);
-        let table = document.getElementById("tBody");
-        table.innerHTML="";
-        for(let user of users.data){
-            table.innerHTML += `
-            <tr>
-                <td>${user.id}</td>
-                <td>${user.first_name}</td>
-                <td>${user.last_name}</td>
-                <td>${user.email}</td>
-                <td>img src="${user.avatar}" class="rounded-circle" alt="avatar" style="widh:60px"</td>
-            </tr>`
-        }
-    })
-    .catch((error) => {
-        console.error("Error: ",error);
-        alert(error);
-    })
-}
+function busquedaUsuarios(page = 1) {
 
-function busquedaUsuarios(page=1){
-    //console.log("Funcion boton");
-    //fetch(`https://reqresMALITO.in/api/users?page=${page}`)
     fetch(`https://reqres.in/api/users?page=${page}`)
-    .then(response => response.json())
-    .then(users => {
-        console.log(users)
-    })
-    .catch(error => {
-        console.log("La solicitud causo error: ",error)
+        .then(response => response.json())
+        .then(users => {
+            console.log(users.data);
+
+            let tBody = document.getElementById("tBody");
+
+            for (let user of users.data) {
+                console.log("User: ", user);
+                tBody.innerHTML += `
+                <tr class="lostd">
+                    <td>${user.id}</td>
+                    <td>${user.first_name}</td>
+                    <td>${user.last_name}</td>
+                    <td>${user.email}</td>
+                    <td> <img src = "${user.avatar}" class="rounded-circle" alt="avatar"> </td>
+                <tr> `;
+            }
+        })
+
+        .catch(error => {
+            console.log("No se puede procesar error: ", error);
+            alert(error);
+        })
     }
-
-    )
-}
