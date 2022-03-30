2ec644548e3b6c0ea6a2e311379d99efcaca323d
MarcoBotelloM <marco.botmar@gmail.com>
2022-03-30 10:07:41 -0600

    feat: Archivos JS0 y Java

---
+++ MarcoBotelloM/CH10/JS10/sum.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+    return a + b;
+  }
+  
+  module.exports = sum
+
+  const sum = require ("./sum") ;
+
+  test ("Sumamos 1 + 2 y esperamos 3" , ()=>{
+      expect( sum( 1 , 2 )).toBe(3);
+  })
