e0555437bb5bbfb1fc6d438c38fbc7be046ebe81
MarianaVives <marianavives123@gmail.com>
2022-04-05 13:01:59 -0500

    Add java05, 06 y 07

---
+++ MarianaVives/java/java06/src/principal/Main.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package principal;
+
+/**
+ * @author Carlos
+ *5 abr. 2022
+ */
+
+//Ejercicio 1
+//public class Main {
+//	public static void main(String[] args) {
+//		int numero = 10;
+//		long factorial = 1;
+//
+//		for (int i = 1; i <= numero; i++) {
+//			factorial *= i;
+//		}
+//		System.out.println("El factorial de " + numero +" es: " + factorial);
+//	}
+//}
+
+
+//Ejercicio 2
+
+public class Main {
+	public static void main(String[] args) {
+		int num = 4, factorial=1, contador = 0; 		 		 
+		//Factorial con el ciclo for 		
+//		for (int i = 0; i < num; i++) { 		
+//			factorial*= i+1; 		
+//			} 		 		
+		//Factorial con ciclo while 		
+			while(contador < num) { 			
+				factorial*= contador+1; 			
+				contador++; 		
+				} 		 		
+			}}
+	
+//System.out.println("El factorial de " + num + " es: " + factorial);
+
+
+//Ejercicio 3
---
+++ MarianaVives/java/java07/src/mx/org/generation/Aritmetica.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package mx.org.generation;
+
+/**
+ * @author Carlos
+ *5 abr. 2022
+ */
+public class Aritmetica {
+
+}
