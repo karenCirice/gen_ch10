78edb4acd675f9a63833f495eb6dd26de57c8db7
LaloCLZ19 <camposeduardo50@gmail.com>
2022-04-05 00:18:55 -0500

    Feat: Se agrega carpeta Java con ejercicios

---
+++ LaloCLZ19/java/Java06/src/principal/Main.java
@@ -0,0 +1,54 @@
+package principal;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int contador = 0;
+		
+/*		while(contador <= 5) {
+			System.out.println("Valor de contador: "+contador);
+			contador++;
+		}*/
+		
+		//reinicio de variable
+		//contador = 0;
+		//uso del do while
+//		do
+//		{
+//			System.out.println("Valor del contador: "+contador);
+//			contador++;
+//		}while(contador<=5);
+		
+		//for
+//		for (int i = 0; i <= 5; i++) {
+//			System.out.println("Valor de i: "+i);
+//		}
+		
+		//uso de la palabra reservada break
+		
+//		for (int i = 1; i <= 5; i++) {
+//			System.out.println("Numero: "+i);
+//			if (i > 1) {
+//				System.out.println("Salida del texto");
+//				break;
+//			}
+//		}
+		
+		//uso de la palabra reservada continue
+//		for (int i = 1; i <= 5; i++) {
+//			if (i != 3) {
+//				System.out.println("Indicacion 1: "+i);
+//				continue;
+//			}
+//			System.out.println("Indicacion numero 2");
+//		}
+		
+		
+		
+	}
+
+}
