2a4086a775447ef8c6ae9a8b95eb592ea180cfbf
Victor Lopez <campvictor51@gmail.com>
2022-04-05 17:49:48 -0500

    FEAT: se agrego la carpeta java06_FizzBuzz con ejercicio

---
+++ victormanuellopez/java/java06_FizzBuzz/src/java06_FizzBuzz/Main.java
@@ -0,0 +1,20 @@
+package java06_FizzBuzz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1; i<=20; i++) {
+			if(i%3 == 0 && i%5 != 0) {
+				System.out.println("Fizz");
+			}else if(i%5 == 0 && i%3 != 0) {
+				System.out.println("Buzz");
+			}else if(i%3 == 0 && i%5 == 0) {
+				System.out.println("FizzBuzz");
+			}else {
+				System.out.println(i);
+			}
+		}
+	}
+}
