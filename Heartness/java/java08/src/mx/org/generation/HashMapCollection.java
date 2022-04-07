c7f84d61adccb55750bc90fcc7a82bcb54208923
Eddie <eddie.edward97@gmail.com>
2022-04-07 17:08:50 -0500

    FEAT: java08 y java09

---
+++ Heartness/java/java08/src/mx/org/generation/HashMapCollection.java
@@ -0,0 +1,22 @@
+package mx.org.generation;
+
+import java.util.HashMap;
+
+public class HashMapCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> postres = new HashMap<Integer, String>();
+		
+		postres.put(1, "Cheescake");
+		postres.put(2, "Blizzard");
+		postres.put(3, "Niño Envuelto");
+		postres.put(4, "milt tea con tapioca");
+		postres.put(5, "Jericalla");
+		postres.put(6, "Totis");
+		postres.put(7, "Esquites");
+		System.out.println(postres);
+	}
+
+}
