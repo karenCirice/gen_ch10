269f44f9e80289eefbe5dbd5fdff93ca9ba66e87
DafneDiaz98 <dafne.diaz.aldama@gmail.com>
2022-04-07 17:35:41 -0500

    Actualizacion de java

---
+++ DafneDIaz98/java08/src/java08/ArrayListCollection.java
@@ -0,0 +1,64 @@
+package java08;
+
+public class ArrayListCollection {
+	import java.util.ArrayList;
+	import java.util.Iterator;
+
+	public class ArrayListCollection {
+
+		public static void main(String[] args) {
+			// Una lista es una colección ordenada
+			//la cual permite elementos duplicados
+			//El contenido de ArrayList puede ser dinámico
+			//Ósea que puede amuentar su tamaño 
+			
+			//Definimos un array de tipo String se tiene
+			//que impostar el util.Arraylist 
+			//Entre <> se define el tipo de dato envolvente
+			ArrayList<String> cars = new ArrayList<String>();
+			cars.add("Volvo");
+			cars.add("BMW");
+			cars.add("Ford");
+			cars.add("Mazda");
+			cars.add("Tsuru");
+			System.out.println(cars);
+			
+			//Obtenemos la longitud del ArrayList
+			int size = cars.size();
+			System.out.println(size);
+			
+			//Obtenemos un dato
+			String miAuto = cars.get(4);
+			System.out.println("Mi auto romántico: "+miAuto);
+		
+			//Eliminar un elemento
+			cars.remove(2);
+			System.out.println(cars);
+			
+			//Interar todos los elementos en ciclo for.
+			
+			for (int i =0; i<cars.size(); i++) {
+				System.out.println("Te regalo el auto: "+cars.get(i));
+			}
+			
+			//Segunda forma de interar 
+			//Otra forma de interar con for 
+			//solo creas una nueva avrible y le dices de dodne jale la info
+			for(String car : cars) {
+				System.out.println("Auto "+car);
+			}
+			
+			//Tercera forma de interar usando forEach
+			cars.forEach(car ->{
+				System.out.println("Autos forEach: "+ car);
+			});
+			
+			//Inicializar un Array usando una subclase anonima
+			ArrayList<Integer> calificaciones =
+					new ArrayList<Integer>() {{add(9);add(10);add(8);}};
+		}
+		
+
+	}
+
+}
---
+++ DafneDIaz98/java09/src/java09/Oop_prueba.java
@@ -0,0 +1,38 @@
+package java09;
+
+public class Oop_prueba {
+	public static void main (String[] args{
+	int atributoX; //esto es una variable, pero se llama atributo
+	int atributoY; ////esto es una variable, pero se llama atributo
+		/*
+		 * definimos 2 variables locales
+		 * cada atributo es una variable
+		 * solo se considera atributo si es miembro de una clase
+		 * var no se puede utilizar en atributos
+		 */
+		
+	int numA; //esto se considera variable
+	int numB; //esto se considera variable
+	
+	Aritmetica operaciones = new Aritmetica();
+	int resultado = operaciones.sumarConArgumentos(4,6);
+	Systme.out.ptintln(resultado);
+	System.out.println(operaciones.sumarConReturn());
+	
+	Aritmetica calculadora= new Aritmetica();
+	package mx.org.generatio;
+	public class Aritmetica{
+		//definimos
+		int a;
+		int b;
+		//el nombre del constructor debe ser identico a la clase
+		Aritmetica(){
+			
+			/*para que pueda se usado este metodo en static main
+			este metodo tambien debe ser de tipo static*/
+			
+			void sumar() {
+				
+			}
+
+}
