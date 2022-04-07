b5a4f4a17d55f0c4631006c8f52887fd65b794e1
Leonardo Marín López <leo.lml@gmail.com>
2022-04-07 17:46:41 -0500

    CHORE: continuación del tema de Colecciones; ArrayList, HashSet y HashMap

---
+++ LeonardoMarinL/java/java08/src/mx/org/generation/HashSetCollection.java
@@ -0,0 +1,46 @@
+package mx.org.generation;
+
+import java.util.HashSet;
+
+public class HashSetCollection {
+
+	public static void main(String[] args) {
+		// Un SET es una colección NO ORDENADA de elementos
+		//NO se permite elementos duplicados
+		//Por lo que cualquier elemento duplicado no se insertará
+		
+		//Definimos un HashSet de tipo String
+		HashSet<String> superHeroes = new HashSet<String>();
+		
+		//Agregamos valores al HashSet
+		superHeroes.add("Calcetín Conrombos Man");
+		superHeroes.add("Thor");
+		superHeroes.add("LaddyBug");
+		superHeroes.add("Porfirio Cadenas");
+		superHeroes.add("Caliman");
+		superHeroes.add("Chapulín Colorado");
+		System.out.println(superHeroes);
+		
+		//Desplegar la longitud
+		int longitud = superHeroes.size();
+		System.out.println("Longitud Total: "+longitud);
+		
+		//Verificar si contiene un elemento
+		boolean contieneNombre = superHeroes.contains("Caliman");
+		System.out.println("¿Contiene Caliman? "+contieneNombre);
+		
+		//Removemos un elemento
+		superHeroes.remove("Thor");
+		System.out.println(superHeroes);
+		
+		//Obtener el HasCode de los elementos HashSet
+//		for (String superHeroe: superHeroes) {
+//			System.out.println(System.identityHashCode(superHeroes));
+//		}
+		
+		//iteramos los valores de superHeroes
+		superHeroes.forEach(heroe -> System.out.println("Heroe: "+heroe));
+
+	}
+
+}
