0088e92462a00337ff3164dc7ef5e7af7083fc7a
AlmaHernandezS <hernandezsanchezalmalizette@gmail.com>
2022-04-08 18:30:42 -0500

    FEAT: Carpetas Java07-08-09

---
+++ AlmaHernandezS/java/java07/src/mx/org/generation/AritmeticaPrueba.java
@@ -0,0 +1,33 @@
+package mx.org.generation;
+
+import java.util.Scanner;
+
+public class AritmeticaPrueba {
+	
+	public static void main(String[] args) {
+		Aritmetica operacion = new Aritmetica();
+		operacion.a = 5;
+		operacion.b = 6;
+		operacion.sumar();
+		
+		int resultado = operacion.sumarConReturn();
+		System.out.println(resultado);
+		
+		int resultadoParametro = operacion.sumarConArgumentos(9,6);
+		System.out.println("Resultado con parametros: " +resultadoParametro);
+		
+		System.out.println("N. random: " +operacion.usoDeRandom(100));
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce tu edad: ");
+		int i = sc.nextInt();
+		System.out.println("Introduce tu nombre: ");
+		String name = sc.next();
+		System.out.println("Introduce tu sexo: ");
+		char sexo = sc.next().charAt(0);
+		System.out.println("Tu edad es: " +i);
+		System.out.println("Tu nombre es: " +name);
+		System.out.println("Tu sexo es: " +sexo);
+	}
+
+}
---
+++ AlmaHernandezS/java/java08/src/mx/org/generation/HashMapCollection.java
@@ -0,0 +1,40 @@
+package mx.org.generation;
+
+import java.util.HashMap;
+
+public class HashMapCollection {
+
+	public static void main(String[] args) {
+		//Un HashMap relaciona una llave (Key) con un valor
+				// llave -valor
+				//formando una especie de tabla de datos
+				
+				//Definimos una colección de tipo Integer, String
+				HashMap<Integer, String> postres = new HashMap<Integer, String>();
+				
+				//Agregamos datos a mi colección
+				postres.put(10, "Cheesecake");
+				postres.put(21, "Blizzard");
+				postres.put(34, "Niño envuelto");
+				postres.put(44, "milk tea con tapioca");
+				postres.put(5, "Jericalla");
+				postres.put(6, "Esquites");
+				System.out.println(postres);
+				postres.put(5, "Totis");
+				postres.put(7, "Esquites");
+				System.out.println(postres);
+				
+				//Despegamos la longitud del HashMap
+				int longitud = postres.size();
+				System.out.println("Longitud total :"+longitud);
+				//Desplegamos el postre totis
+				String postreGera = postres.get(5);
+				System.out.println("Postre que trajo Gera: " +postreGera);
+				
+				//Removemos el postre de Alondra :P
+				postres.remove(10);
+				System.out.println(postres);
+
+	}
+
+}
