0ecccf17306d02b0da855191bdf276cf2dd5cc5c
Marcos Sanchez <marcos.sanzlara@gmail.com>
2022-04-17 22:17:06 -0500

    FEAT: Se agregan ejercicios Java

---
+++ MarcosSanchezAV/java/java07/src/clases/PruebaPersona.java
@@ -0,0 +1,20 @@
+package clases;
+
+public class PruebaPersona {
+
+	public static void main(String[] args) {
+		Persona persona1 = new Persona();
+		persona1.nombre = "Manuel";
+		persona1.apellido = "Torres";
+		persona1.desplegarInformacion();
+		// @ referencia del objeto en memoria
+		System.out.println("Dato persona1 : " + persona1);
+
+		Persona persona2 = new Persona();
+//		persona2.nombre = "Alejandra";
+//		persona2.apellido = "Molina";
+		persona2.desplegarInformacion();
+
+	}
+
+}
